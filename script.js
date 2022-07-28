fd25751b30208c70c1415f608c4444dab281ca47
Danil Murzin <danil.murzin.200026@gmail.com>
2022-07-28 12:08:39 +0300

    add first homework

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = 'Some title';
+let screens;
+let screenPrice;
+const rollback = 'test'; 
+let fullPrice = 300;
+let adaptive = true;
+
+alert(title);
+console.log('rollback: ', rollback);
