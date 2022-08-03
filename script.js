ec89a854399458f244da93d47b0ea88ea7df3d4e
Danil Murzin <danil.murzin.200026@gmail.com>
2022-08-03 18:37:22 +0300

    fix homework

--- script.js
+++ script.js
@@ -1,40 +1,43 @@
 'use strict';
 
-const title = prompt('Введите название Вашего проекта...'),
-screens = prompt('Введите типы экранов для разработки'),
-screenPrice = +prompt('Сколько будет стоить данная работа?'),
-rollback = 50,
-adaptive = confirm('Нужен ли адаптив на сайте?'),
-service1 = prompt('Какой дополнительный тип услуги нужен?'),
-servicePrice1 = +prompt('Сколько это будет стоить?'),
-service2 = prompt('Какой дополнительный тип услуги нужен?'),
-servicePrice2 = +prompt('Сколько это будет стоить?');
+let title = prompt('Введите название Вашего проекта...');
+const screens = prompt('Введите типы экранов для разработки'),
+  screenPrice = +prompt('Сколько будет стоить данная работа?'),
+  rollback = 50,
+  adaptive = confirm('Нужен ли адаптив на сайте?'),
+  service1 = prompt('Какой дополнительный тип услуги нужен?'),
+  servicePrice1 = +prompt('Сколько это будет стоить?'),
+  service2 = prompt('Какой дополнительный тип услуги нужен?'),
+  servicePrice2 = +prompt('Сколько это будет стоить?');
 
 // 1
 const getAllServicePrices = function (price1, price2) {
   return price1 + price2;
 };
 
-let allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
+const allServicePrices = getAllServicePrices(servicePrice1, servicePrice2);
 
 // 2
 function getFullPrice(screenPrice, allServicePrices) {
   return screenPrice + allServicePrices;
 }
 
-let fullPrice = getFullPrice(screenPrice, allServicePrices);
+const fullPrice = getFullPrice(screenPrice, allServicePrices);
 
 // 3
-const getTitle = (title) => {
-  return title[0].toUpperCase() + title.slice(1).toLowerCase();
+const getTitle = (str) => {
+  const title = str.trim().toLowerCase();
+  return title[0].toUpperCase() + title.substr(1);
 };
+
+title = getTitle(title);
 
 // 4
 const getServicePercentPrices = (fullPrice, rollback) => {
-  return fullPrice - rollback;
+  return fullPrice - (fullPrice * rollback) / 100;
 };
 
-let servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
+const servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
 
 const getRollbackMessage = (price) => {
   if (price >= 30000) {
@@ -57,7 +60,7 @@ showTypeOf(title);
 showTypeOf(screenPrice);
 showTypeOf(adaptive);
 
-console.log(getTitle(title));
-console.log('screens: ', screens);
-console.log(getRollbackMessage(getFullPrice(screenPrice)));
+console.log(title);
+console.log(screens.toLocaleLowerCase().split(', '));
+console.log(getRollbackMessage(fullPrice));
 console.log(servicePercentPrice);
