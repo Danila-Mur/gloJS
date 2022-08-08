8a3c9adee6d0c5c4fa981a5a718e12a1c32a2aeb
Danil Murzin <danil.murzin.200026@gmail.com>
2022-08-08 12:03:46 +0300

    add homework for lesson07

--- script.js
+++ script.js
@@ -1,95 +1,103 @@
 'use strict';
 
-let title,
-  screenPrice,
-  screens,
-  adaptive,
-  service1,
-  servicePrice1,
-  service2,
-  servicePrice2;
-const rollback = 50;
-
-const isNumber = (num) => {
-  return !isNaN(parseFloat(num)) && isFinite(num) && num.trim() === num;
-};
-
-const asking = () => {
-  title = prompt('Введите название Вашего проекта...', 'Калькулятор верстки');
-  screens = prompt(
-    'Введите типы экранов для разработки',
-    'Desktop, tablet, mobile'
-  );
-
-  do {
-    screenPrice = prompt('Сколько будет стоить данная работа?', 15000);
-  } while (!isNumber(screenPrice));
-
-  adaptive = confirm('Нужен ли адаптив на сайте?');
-};
-
-const getAllServicePrices = function () {
-  let sum = 0;
-  let price;
-
-  for (let i = 0; i < 2; i++) {
-    if (i === 0) {
-      service1 = prompt('Какой дополнительный тип услуги нужен?');
-    } else if (i === 1) {
-      service2 = prompt('Какой дополнительный тип услуги нужен?');
-    }
+const appData = {
+  title: '',
+  screens: '',
+  screenPrice: 0,
+  adaptive: false,
+  service1: '',
+  servicePrice1: 0,
+  service2: '',
+  servicePrice2: 0,
+  rollback: 50,
+  start: function () {
+    appData.asking();
+    appData.allServicePrices = appData.getAllServicePrices();
+    appData.fullPrice = appData.getFullPrice();
+    appData.title = appData.getTitle();
+    appData.servicePercentPrice = appData.getServicePercentPrices();
+    appData.logger();
+  },
+  logger: function () {
+    console.log(appData.title);
+    console.log(appData.screens.toLocaleLowerCase().split(', '));
+    console.log(appData.getRollbackMessage(appData.fullPrice));
+    console.log(appData.servicePercentPrice);
+  },
+  asking: function () {
+    appData.title = prompt(
+      'Введите название Вашего проекта...',
+      'Калькулятор верстки'
+    );
+    appData.screens = prompt(
+      'Введите типы экранов для разработки',
+      'Desktop, tablet, mobile'
+    );
 
     do {
-      price = prompt('Сколько это будет стоить?', '10000');
-    } while (!isNumber(price));
-    sum += +price;
-  }
+      appData.screenPrice = prompt(
+        'Сколько будет стоить данная работа?',
+        15000
+      );
+    } while (!appData.isNumber(appData.screenPrice));
 
-  return sum;
+    appData.adaptive = confirm('Нужен ли адаптив на сайте?');
+  },
+  isNumber: function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num) && num.trim() === num;
+  },
+  getAllServicePrices: function () {
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++) {
+      let price = 0;
+
+      if (i === 0) {
+        appData.service1 = prompt('Какой дополнительный тип услуги нужен?');
+      } else if (i === 1) {
+        appData.service2 = prompt('Какой дополнительный тип услуги нужен?');
+      }
+
+      do {
+        price = prompt('Сколько это будет стоить?', '10000');
+      } while (!appData.isNumber(price));
+      sum += +price;
+    }
+
+    return sum;
+  },
+  getFullPrice: function () {
+    return +appData.screenPrice + appData.allServicePrices;
+  },
+  getTitle: function () {
+    return (
+      appData.title.trim()[0].toUpperCase() +
+      appData.title.trim().substr(1).toLowerCase()
+    );
+  },
+  getServicePercentPrices: function () {
+    return appData.fullPrice - (appData.fullPrice * appData.rollback) / 100;
+  },
+  getRollbackMessage: function (price) {
+    if (price >= 30000) {
+      return 'Скидка 10%';
+    } else if (price >= 15000) {
+      return 'Скидка 5%';
+    } else if (price >= 0) {
+      return 'Скидка не предусмотрена';
+    } else {
+      return 'Что то пошло не так';
+    }
+  },
 };
 
-function getFullPrice(screenPrice, allServicePrices) {
-  return +screenPrice + allServicePrices;
-}
+appData.start();
+// appData.allServicePrices = appData.getAllServicePrices();
+// appData.fullPrice = appData.getFullPrice();
+// appData.title = appData.getTitle();
+// appData.servicePercentPrice = appData.getServicePercentPrices();
 
-const getTitle = (str) => {
-  const title = str.trim().toLowerCase();
-  return title[0].toUpperCase() + title.substr(1);
-};
+// console.log(appData.fullPrice);
+// console.log(appData.servicePercentPrice);
 
-const getServicePercentPrices = (fullPrice, rollback) => {
-  return fullPrice - (fullPrice * rollback) / 100;
-};
-
-const getRollbackMessage = (price) => {
-  if (price >= 30000) {
-    return 'Скидка 10%';
-  } else if (price >= 15000) {
-    return 'Скидка 5%';
-  } else if (price >= 0) {
-    return 'Скидка не предусмотрена';
-  } else {
-    return 'Что то пошло не так';
-  }
-};
-
-const showTypeOf = (variable) => {
-  console.log(variable, typeof variable);
-};
-
-asking();
-const allServicePrices = getAllServicePrices();
-const fullPrice = getFullPrice(screenPrice, allServicePrices);
-title = getTitle(title);
-const servicePercentPrice = getServicePercentPrices(fullPrice, rollback);
-
-showTypeOf(title);
-showTypeOf(screenPrice);
-showTypeOf(adaptive);
-
-console.log('screenPrice:', screenPrice);
-
-console.log(title);
-console.log(screens.toLocaleLowerCase().split(', '));
-console.log(getRollbackMessage(fullPrice));
-console.log(servicePercentPrice);
+// console.log(appData.getAllServicePrices());
